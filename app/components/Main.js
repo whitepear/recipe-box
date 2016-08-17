4f71bdd80a2b691fcc0b30e9ec900df901c69e44
John Doe <webdevnorth@gmail.com>
2016-08-17 15:19:05 +0100

    implemented render of localStorage info within Main.js

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,10 +1,28 @@
 var React = require('react');
 var PropTypes = React.PropTypes;
+var key = 0;
+
+function keyGen () {
+	key++;
+	return key;
+}
 
 function Main (props) {
 	console.log(props.recipes);
+	
 	return (
-		<div>Main.</div>
+		<div className="accordion-group">
+		  {Object.keys(props.recipes).map(function (recipeName) {
+				return <div className="accordion-unit" key={keyGen()}>
+			    <div className="accordion-header">{recipeName}</div>
+			    <div className="accordion-body">
+				    {JSON.parse(props.recipes[recipeName]).map(function(ingredient) {
+				    	return <div>{ingredient}</div>
+				    })} 	
+			    </div>	
+			  </div>	
+			})}
+		</div>
 	)
 }
 
