fddaf1e2f1e450133e923c1aa15dd5bb38899f1a
John Doe <webdevnorth@gmail.com>
2016-08-18 21:48:19 +0100

    implemented panel opening functionality to Accordion

--- app/components/Accordion.js
+++ app/components/Accordion.js
@@ -6,12 +6,32 @@ var Accordion = React.createClass({
 		recipeName: PropTypes.string.isRequired,
 		ingredients: PropTypes.string.isRequired
 	},
+	getInitialState: function () {
+		return {
+			open: false
+		}
+	},
+	toggleOpen: function () {		
+		this.setState({			
+			open: !(this.state.open)
+		}, function () {
+			console.log(this.state);
+		});		
+	},
+	getOpenHeight: function () {
+		// Refactor to apply classes rather than inline-styles
+		if(this.state.open) {
+			return "accordion-body open-body";			
+		} else {
+			return "accordion-body";
+		}
+	},
 	render: function () {
 		var key = 0;
 		return (
 			<div className="accordion-unit">
-				<div className="accordion-header">{this.props.recipeName}</div>
-				<div className="accordion-body">
+				<div className="accordion-header" onClick={this.toggleOpen}>{this.props.recipeName}</div>
+				<div className={this.getOpenHeight()} >
 					{JSON.parse(this.props.ingredients).map(function (ingredient) {
 						return <div key={key++}>{ingredient}</div>
 					})}
