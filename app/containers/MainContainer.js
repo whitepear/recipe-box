8f12728ff599f36aa97e84b64ec4d4600582c0cf
John Doe <webdevnorth@gmail.com>
2016-08-24 00:20:35 +0100

    implemented mobile styling, changed default browserStorage value

--- app/containers/MainContainer.js
+++ app/containers/MainContainer.js
@@ -7,8 +7,8 @@ var MainContainer = React.createClass({
 		// If localStorage is empty, push default recipes into localStorage
 		if (Object.keys(localStorage).length === 0) {			
 			localStorage.setItem('Pumpkin Pie', JSON.stringify(['Pumpkin Puree', 'Sweetened Condensed Milk', 'Eggs', 'Pumpkin Pie Spice', 'Pie Crust']));
-			localStorage.setItem('Spaghetti', JSON.stringify(['Noodles', 'Tomato Sauce', '(Optional) Meatballs']));
-			localStorage.setItem('Onion Pie', JSON.stringify(['Onion', 'Pie Crust', 'Sounds Yummy right?']));			
+			localStorage.setItem('Spaghetti', JSON.stringify(['Pasta', 'Tomato Sauce', 'Meatballs']));
+			localStorage.setItem('Onion Pie', JSON.stringify(['Onion', 'Pie Crust', 'Sugar', 'Eggs']));			
 		}		
 
 		return {
